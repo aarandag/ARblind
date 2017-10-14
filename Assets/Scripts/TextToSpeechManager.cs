f1ef38a3a0091b62d44335f9e443da825971c150
Cris21395 <Cristian.Gomez2@alu.uclm.es>
2017-10-14 15:00:56 +0200

    Upload Assets and ProjectSettings folders

---
+++ Assets/Scripts/TextToSpeechManager.cs
@@ -0,0 +1,17 @@
+﻿using HoloToolkit.Unity;
+using UnityEngine;
+
+public class TextToSpeechManager : MonoBehaviour {
+
+    private TextToSpeech textToSpeech;
+
+    private void Awake()
+    {
+        textToSpeech = GetComponent<TextToSpeech>();
+    }
+
+    public void SpeakTime()
+    {
+        textToSpeech.StartSpeaking("No! You can walk altough you should take care");
+    }
+}
