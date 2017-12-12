581e4114dfec9260027dea80bd84364eb7f9c198
Daniel Pozo Romero <daniel.pozo@furiouskoalas.com>
2017-12-12 19:35:37 +0100

    object's number fixed

--- Assets/Scripts/TextToSpeechManager.cs
+++ Assets/Scripts/TextToSpeechManager.cs
@@ -26,45 +26,24 @@ public class TextToSpeechManager : MonoBehaviour {
     public void IsThereAnythingNextToMe()
     {
         PlayVisualizer.Instance.Query_Shape_FindShapeHalfDims("All Surfaces");
-        StartCoroutine(WaitForObtainNumberOfObjects());
-        if (n_objects <= 0)
-        {
-            textToSpeech.StartSpeaking("There is not anything next to me");
-        }
-        else
-        {
-            textToSpeech.StartSpeaking("There is something next to me");
-        }
+        StartCoroutine(WaitForObtainNumberOfObjects("Anything"));
+        
     }
 
     // Response whether or not there is some chair next to me
     public void IsThereAnyChairNextToMe()
     {
         PlayVisualizer.Instance.Query_Shape_FindShapeHalfDims("Sittable");
-        StartCoroutine(WaitForObtainNumberOfObjects());
-        if (n_objects <= 0)
-        {
-            textToSpeech.StartSpeaking("There is no any chair next to me");
-        }
-        else
-        {
-            textToSpeech.StartSpeaking("There is some chairs next to me");
-        }
+        StartCoroutine(WaitForObtainNumberOfObjects("Sittable"));
+        
     }
 
     // Response whether or not there is some table next to me
     public void IsThereAnyTableNextToMe()
     {
         PlayVisualizer.Instance.Query_Shape_FindShapeHalfDims("Large Empty Surface");
-        StartCoroutine(WaitForObtainNumberOfObjects());
-        if (n_objects <= 0)
-        {
-            textToSpeech.StartSpeaking("There is no table next to me");
-        }
-        else
-        {
-            textToSpeech.StartSpeaking("There is some tables next to me");
-        }
+        StartCoroutine(WaitForObtainNumberOfObjects("Table"));
+        
     }
 
     // Response the distance between person and object
@@ -89,11 +68,46 @@ public class TextToSpeechManager : MonoBehaviour {
     /// Wait for 3 seconds to obtain the number of the objects that have been found
     /// </summary>
     /// <returns></returns>
-    private IEnumerator WaitForObtainNumberOfObjects()
+    private IEnumerator WaitForObtainNumberOfObjects(string type)
     {
         yield return new WaitForSeconds(3.0f);
         string number_shapes = ScanDisplay.text;
         var resultString = Regex.Match(number_shapes, @"\d+").Value; // \d + is the regex for an integer number
         n_objects = int.Parse(resultString);
+        switch (type)
+        {
+            case "Anything":
+                if (n_objects <= 0)
+                {
+                    textToSpeech.StartSpeaking("There is not anything next to me");
+                }
+                else
+                {
+                    textToSpeech.StartSpeaking("There is something next to me");
+                }
+                break;
+            case "Table":
+                if (n_objects <= 0)
+                {
+                    textToSpeech.StartSpeaking("There is no table next to me");
+                }
+                else
+                {
+                    textToSpeech.StartSpeaking("There is some tables next to me");
+                }
+                break;
+            case "Sittable":
+                if (n_objects <= 0)
+                {
+                    textToSpeech.StartSpeaking("There is no any chair next to me");
+                }
+                else
+                {
+                    textToSpeech.StartSpeaking("There is some chairs next to me");
+                }
+                break;
+
+        }
+
     }
 }
